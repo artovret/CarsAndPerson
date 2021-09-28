bff35a2cdfc95e7e23b9b44c28309aa17639dc02
19224031 <AAndreTitov@sberbank.ru>
2021-09-28 23:30:44 +0300

    init

--- src/main/java/com/example/cars/dto/PersonDto.java
+++ src/main/java/com/example/cars/dto/PersonDto.java
@@ -15,10 +15,10 @@ import java.util.Date;
 public class PersonDto {
     private Long id;
     private String name;
-    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd.MM.yyyy")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
     private Date birthdate;
 
-    public static PersonDto fromPerson(Person person){
+    public static PersonDto fromPerson(Person person) {
         PersonDto personDto = new PersonDto();
         personDto.setId(person.getId());
         personDto.setName(person.getName());
@@ -27,13 +27,14 @@ public class PersonDto {
     }
 
     public static Person toPerson(PersonDto personDto) throws ValidateException {
-        if (personDto.getBirthdate().after(new Date()) && personDto.getName()==null && personDto.getId()==null) {
+        if (personDto.getBirthdate().after(new Date()) || personDto.getName() == null || personDto.getId() == null) {
+            log.warn("Person");
             throw new ValidateException("Ошибка валидации");
         }
-            Person person = new Person();
-            person.setId(personDto.getId());
-            person.setName(personDto.getName());
-            person.setBirthdate(personDto.getBirthdate());
-            return person;
+        Person person = new Person();
+        person.setId(personDto.getId());
+        person.setName(personDto.getName());
+        person.setBirthdate(personDto.getBirthdate());
+        return person;
     }
 }
---
+++ src/main/java/com/example/cars/exceptions/ValidateException.java
@@ -0,0 +1,7 @@
+package com.example.cars.exceptions;
+
+public class ValidateException extends Exception {
+    public ValidateException(String message) {
+        super(message);
+    }
+}
